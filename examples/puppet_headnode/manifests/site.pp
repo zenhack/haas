8e0c8ddb4bf3bcc014dc24b6e8baf6d23c3c15e7
Ian Denhardt <ian@zenhack.net>
2014-11-13 17:04:17 -0500

    Fix a handful of small errors.
    * Missing closing brace.
    * Parent directories must first exist in a few cases.
    * /etc/networking/interfaces should be /etc/network/interfaces.
    
    Also, clean up a few whitespace oddities.

--- examples/puppet_headnode/manifests/site.pp
+++ examples/puppet_headnode/manifests/site.pp
@@ -9,6 +9,9 @@ package { [
 }
 
 ## Mount the Ubuntu ISO and copy files ##
+file { "/mnt/iso":
+  ensure => directory,
+} ->
 mount { "/mnt/iso":
   device => "/root/CentOS-6.6-x86_64-minimal.iso",
   fstype  => "iso9660",
@@ -20,7 +23,7 @@ mount { "/mnt/iso":
 ## Files to be updated ##
 
 # set interface configuration
-file { '/etc/networking/interfaces':
+file { '/etc/network/interfaces':
   ensure  => present,
   source  => "/root/haas/examples/puppet_headnode/manifests/static/interfaces",
 }
@@ -28,42 +31,45 @@ file { '/etc/networking/interfaces':
 file { '/etc/default/isc-dhcp-server':
   ensure  => present,
   source  => "/root/haas/examples/puppet_headnode/manifests/static/isc-dhcp-server",
-  require  => Package["isc-dhcp-server"]
+  require => Package["isc-dhcp-server"]
 }
 
 # dhcp configuration
 file { '/etc/dhcp/dhcp.conf':
   ensure  => present,
   source  => "/root/haas/examples/puppet_headnode/manifests/static/dhcp.conf",
-  require  => Package["isc-dhcp-server"],
+  require => Package["isc-dhcp-server"],
   notify  => Service["isc-dhcp-server"]
 }
 
 # tftp configuration
 file { "/etc/default/tftpd-hpa":
   ensure  => present,
-  require  => Package["tftpd-hpa"],
+  require =>  Package["tftpd-hpa"],
   source  => "/root/haas/examples/puppet_headnode/manifests/static/tftpd-hpa",
 }
 
 file { "/etc/inetd.conf":
   ensure  => present,
-  require  => Package["inetutils-inetd"],
+  require => Package["inetutils-inetd"],
   source  => "/root/haas/examples/puppet_headnode/manifests/static/inetd.conf",
   notify  => Service["tftpd-hpa"]
 }
 
 # pxe configuration
+file { "/var/lib/tftpboot/pxelinux.cfg":
+  ensure => directory,
+} ->
 file { "/var/lib/tftpboot/pxelinux.cfg/default":
   ensure  => present,
   source  => "/root/haas/examples/puppet_headnode/manifests/static/default",
-  require  => Package["tftpd-hpa"],
+  require => Package["tftpd-hpa"],
 }
 
 file { "/var/lib/tftpboot/centos/pxelinux.cfg":
   ensure  => present,
   source  => "/root/haas/examples/puppet_headnode/manifests/static/centos_pxelinux_cfg",
-  require  => Package["tftpd-hpa"],
+  require => Package["tftpd-hpa"],
 }
 
 file { "/var/lib/tftpboot/centos/vmlinuz":
@@ -86,6 +92,7 @@ file { "/var/lib/tftpboot/centos/ks.cfg":
 file { "/usr/local/bin/make_links":
   ensure  => present,
   source  => "/root/haas/examples/puppet_headnode/manifests/static/make_links",
+}
 
 file { "/usr/local/bin/boot_notify.py":
   ensure  => present,
